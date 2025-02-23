ab04c2aaaf4da1395394710d9d4d5b7bf7aa4817
Jonta Sancar <jontasancar@gmail.com>
2025-02-23 13:53:41 -0300

    simplefy the code

--- php-sh.bat
+++ php-sh.bat
@@ -3,8 +3,4 @@ setlocal enabledelayedexpansion
 
 set "HANDLER_PATH=%CD%\server_handler.php"
 
-set "args=%*"
-set "args=!args:--server-handler=!"
-set "args=!args:-SH=!"
-
 php "%HANDLER_PATH%" !args!
