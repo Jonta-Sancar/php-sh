b0d7f4b4ff9a77e44667d7c4101d03dcecc959af
Jonta Sancar <jontasancar@gmail.com>
2025-02-24 03:07:04 -0300

    correção para que o comando funcione globalmente, em qualquer local onde se abrir o terminal

--- php-sh.bat
+++ php-sh.bat
@@ -1,7 +1,10 @@
 @echo off
 setlocal enabledelayedexpansion
 
-set "HANDLER_PATH=%CD%\server_handler.php"
+set "BAT_DIR=%~dp0"
+set "BAT_DIR=%BAT_DIR:~0,-1%"
+
+set "HANDLER_PATH=%BAT_DIR%\server_handler.php"
 
 set "args=%*"
 
